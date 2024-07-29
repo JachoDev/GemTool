6b77a2a714e85aceb0bda93524ab2fa03e6f630e
JachoDev <javier.a.r.d28@gmail.com>
2024-07-29 12:36:05 -0600

    first very_good repositories

---
+++ packages/local_storage_ticket_api/lib/src/local_storage_ticket_api.dart
@@ -0,0 +1,7 @@
+/// {@template local_storage_ticket_api}
+/// A Flutter implementation of the TicketApi that uses local storage.
+/// {@endtemplate}
+class LocalStorageTicketApi {
+  /// {@macro local_storage_ticket_api}
+  const LocalStorageTicketApi();
+}
---
+++ packages/local_storage_ticket_api/test/src/local_storage_ticket_api_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:local_storage_ticket_api/local_storage_ticket_api.dart';
+
+void main() {
+  group('LocalStorageTicketApi', () {
+    test('can be instantiated', () {
+      expect(LocalStorageTicketApi(), isNotNull);
+    });
+  });
+}
